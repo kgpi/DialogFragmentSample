f325a434470b3d4354e915dd1a03cc8489ced1be
Shobayashi <kgpi@diana.dti.ne.jp>
2015-11-19 00:40:17 +0900

    AlertDialogFragmentサンプル作成

---
+++ DialogFragmentSample/app/src/main/java/com/example/kgpi01/dialogfragmentsample/AlertDialogFragment.java
@@ -0,0 +1,66 @@
+package com.example.kgpi01.dialogfragmentsample;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * 公式リファレンスサンプル
+ *
+ * Created by KGPI01 on 2015/11/18.
+ */
+public class AlertDialogFragment extends DialogFragment {
+
+    private static final String TitleKeyword = "title";
+    private static final String MessageKeyword = "message";
+
+    String title = "";       // タイトル
+    String message = "";     // ダイアログ本文
+    DialogInterface.OnClickListener positiveListener;
+    DialogInterface.OnClickListener negativeListener;
+
+    /**
+     * Create a new instance of MyDialogFragment, providing "num"
+     * as an argument.
+     *
+     * Bundle : オブジェクトの入れ物、入れたオブジェクトはonCreateで取り出す
+     */
+    public static AlertDialogFragment newInstance(String title, String message, DialogInterface.OnClickListener positiveListener,
+                                                                                DialogInterface.OnClickListener negativeListener) {
+        AlertDialogFragment f = new AlertDialogFragment();
+        f.setOnClickListener(positiveListener, negativeListener);
+        f.setCancelable(false);
+
+        Bundle args = new Bundle();
+        args.putString(TitleKeyword, title);
+        args.putString(MessageKeyword, message);
+        f.setArguments(args);
+
+        return f;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(getArguments() != null) {
+            title = getArguments().getString(TitleKeyword);
+            message = getArguments().getString(MessageKeyword);
+        }
+
+        return new AlertDialog.Builder(getActivity())
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(getString(R.string.dialog_ok), positiveListener)
+            .setNegativeButton(getString(R.string.dialog_cancel), negativeListener)
+            .create();
+    }
+
+    public void setOnClickListener(DialogInterface.OnClickListener positiveListener, DialogInterface.OnClickListener negativeListener) {
+        this.positiveListener = positiveListener;
+        this.negativeListener = negativeListener;
+    }
+
+}
--- DialogFragmentSample/app/src/main/java/com/example/kgpi01/dialogfragmentsample/MainActivity.java
+++ DialogFragmentSample/app/src/main/java/com/example/kgpi01/dialogfragmentsample/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.kgpi01.dialogfragmentsample;
 
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickAlertDialogFragmentButton(View v) {
+        AlertDialogFragment fragment = AlertDialogFragment.newInstance("Title", "Message",
+            new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getApplicationContext(), "OK", Toast.LENGTH_SHORT).show();
+                }
+            }, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_SHORT).show();
+                }
+            }
+        );
+        fragment.show(getFragmentManager(), "alert_dialog");
     }
 }
